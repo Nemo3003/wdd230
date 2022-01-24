89a4541c3ca59f0d15d2d33d66ec762d76edb145
Ulises Mariano Melgarejo <ulimarian2000@gmail.com>
2022-01-24 08:56:56 -0300

    almost done

---
+++ chamber/js/app.js
@@ -0,0 +1,20 @@
+const mainMenu = document.querySelector('.mainMenu');
+const closeMenu = document.querySelector('.closeMenu');
+const openMenu = document.querySelector('.openMenu');
+
+
+
+
+openMenu.addEventListener('click',show);
+closeMenu.addEventListener('click',close);
+
+function show(){
+    mainMenu.style.display = 'flex';
+    mainMenu.style.top = '0';
+}
+function close(){
+    mainMenu.style.top = '-100%';
+}
+
+document.getElementById("modify").innerHTML = document.lastModified
+document.getElementById("year").innerHTML = new Date().getFullYear()
