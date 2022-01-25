7d09416f5a6543ea43c079789218bc69e9644032
Ulises Mariano Melgarejo <ulimarian2000@gmail.com>
2022-01-25 20:04:14 -0300

    dang, it is taking a long time

--- chamber/js/app.js
+++ chamber/js/app.js
@@ -16,5 +16,9 @@ function close(){
     mainMenu.style.top = '-100%';
 }
 
+var today = new Date();
+document.getElementById('displayDateTime').innerHTML=today;
+document.getElementById("displayDateTime").innerHTML = dateTime + ' <br> Day :- ' + daylist[day];
+
 document.getElementById("modify").innerHTML = document.lastModified
 document.getElementById("year").innerHTML = new Date().getFullYear()
