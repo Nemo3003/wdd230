d01879ab9a29c94b2966e74d64ed621e9019affd
Ulises Mariano Melgarejo <ulimarian2000@gmail.com>
2022-01-27 21:45:55 -0300

    almost almost done

--- chamber/js/app.js
+++ chamber/js/app.js
@@ -1,19 +1,5 @@
-const mainMenu = document.querySelector('.mainMenu');
-const closeMenu = document.querySelector('.closeMenu');
-const openMenu = document.querySelector('.openMenu');
 
-openMenu.addEventListener('click',show);
-closeMenu.addEventListener('click',close);
-
-function show(){
-    mainMenu.style.display = 'flex';
-    mainMenu.style.top = '0';
-}
-function close(){
-    mainMenu.style.top = '-100%';
-}
-
-var today = new Date();
+let today = new Date();
 document.getElementById('displayDateTime').innerHTML=today;
 //document.getElementById("displayDateTime").innerHTML = dateTime + ' <br> Day :- ' + daylist[day];
 
