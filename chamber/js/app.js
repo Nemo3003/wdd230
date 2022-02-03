21a07971230ea9152a806f54966c2e41647efc1b
Ulises Mariano Melgarejo <ulimarian2000@gmail.com>
2022-02-03 19:12:04 -0300

    fex more changes and we are ready

--- chamber/js/app.js
+++ chamber/js/app.js
@@ -3,5 +3,13 @@ let today = new Date();
 document.getElementById('displayDateTime').innerHTML=today;
 //document.getElementById("displayDateTime").innerHTML = dateTime + ' <br> Day :- ' + daylist[day];
 
+let banner = document.querySelector('#banner');
+let dayOfWeek = new Date().getDay();
+// if Friday displays this
+if (dayOfWeek == 2 || dayOfWeek == 3) {
+    banner.style.display = "block";
+}
+
 document.getElementById("modify").innerHTML = document.lastModified
 document.getElementById("year").innerHTML = new Date().getFullYear()
+
