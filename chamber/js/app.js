0f671cc14491b7dcd0ea9b40cdc44fcf52c85116
Ulises Mariano Melgarejo <73831325+Nemo3003@users.noreply.github.com>
2022-02-04 07:15:37 -0300

    Update app.js

--- chamber/js/app.js
+++ chamber/js/app.js
@@ -5,7 +5,7 @@ document.getElementById('displayDateTime').innerHTML=today;
 
 let banner = document.querySelector('#banner');
 let dayOfWeek = new Date().getDay();
-// if Friday displays this
+// if Monday or Tuesday displays this
 if (dayOfWeek == 2 || dayOfWeek == 3) {
     banner.style.display = "block";
 }
