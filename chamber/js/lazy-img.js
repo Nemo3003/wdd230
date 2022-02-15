10f07a609872a22e23df63b8a95b28259561a794
Ulises Mariano Melgarejo <ulimarian2000@gmail.com>
2022-02-15 08:02:32 -0300

    lazy load img ready

---
+++ chamber/js/lazy-img.js
@@ -0,0 +1,31 @@
+let imagesToLoad = document.querySelectorAll('img[data-src]');
+
+const imageOptions = {
+    threshold: 1,
+    rootMargin: '0px 0px 50px 0px'
+};
+
+const loadImages = (img) => {
+    img.setAttribute('src', img.getAttribute('data-src'));
+    img.onload = () => {
+        img.removeAttribute('data-src');
+    };
+};
+
+if ('IntersectionObserver' in window) {
+    const observer = new IntersectionObserver((items, observer)  => {
+        items.forEach((item) => {
+            if (item.isIntersecting) {
+                loadImages(item.target);
+                observer.unobserve(item.target);
+            }
+        });
+    });
+    imagesToLoad.forEach((img) => {
+        observer.observe(img);
+    });
+} else {
+    imagesToLoad.forEach((img) => {
+        loadImages(img);
+    });
+}
--- lesson7/js/app.js
+++ lesson7/js/app.js
@@ -1,1 +1,32 @@
-console.log("Hello World!");
+let imagesToLoad = document.querySelectorAll('img[data-src]');
+
+const imgOptions = {
+    threshold: 5,
+    rootMargin: '0px 0px -00px 0px'
+}
+
+const loadImages = (img) => {
+    img.setAttribute('src', img.getAttribute('data-src'));
+    img.onload = () => {
+        img.removeAttribute('data-src');
+    };
+};
+
+if ('IntersectionObserver' in window) {
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach((entry) => {
+            if(entry.isIntersecting) {
+                loadImages(entry.target);
+                observer.unobserve(entry.target);
+            }
+        });
+    });
+
+    imagesToLoad.forEach((image) => {
+        observer.observe(image);
+    });
+} else {
+    imagesToLoad.forEach((image) => {
+        loadImages(image);
+    });
+}
