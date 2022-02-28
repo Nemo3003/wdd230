9d265b6a9d81eed5d681da4a4a2a94c748b7421c
Ulises Mariano Melgarejo <73831325+Nemo3003@users.noreply.github.com>
2022-02-28 09:33:19 -0300

    Add files via upload

---
+++ lesson-9/js/prophets.js
@@ -0,0 +1,32 @@
+const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
+
+fetch(requestURL)
+    .then(function (response) {
+        return response.json();
+
+    })
+    .then(function (jsonObject) {
+        console.table(jsonObject); // temporary checking for valid response and data parsing
+        const prophets = jsonObject['prophets'];
+        for (let i = 0; i < prophets.length; i++) {
+            let card = document.createElement('section');
+            let h2 = document.createElement('h2');
+            let birthdate = document.createElement('p');
+            let birthplace = document.createElement('p');
+            let image = document.createElement('img');
+
+            h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
+            birthdate.textContent = "Date of Birth: " + prophets[i].birthdate;
+            birthplace.textContent = "Place of Birth: " + prophets[i].birthplace;
+
+            card.appendChild(h2);
+            card.appendChild(birthdate);
+            card.appendChild(birthplace);
+            card.appendChild(image);
+
+            image.setAttribute('src', prophets[i].imageurl);
+
+            document.querySelector('div.cards').appendChild(card);
+        };
+    });
+    //Nemo's
