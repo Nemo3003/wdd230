e0c8fccbcc7c66df474de834bbe98385ab84a506
Ulises Mariano Melgarejo <ulimarian2000@gmail.com>
2022-02-28 20:44:01 -0300

    working on fixing various errors, Nav is hard

---
+++ js/app.js
@@ -0,0 +1,15 @@
+
+
+document.querySelector('#now').innerHTML = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format();
+//document.getElementById("displayDateTime").innerHTML = dateTime + ' <br> Day :- ' + daylist[day];
+
+let banner = document.querySelector('#banner');
+let dayOfWeek = new Date().getDay();
+// if Monday or Tuesday displays this
+if (dayOfWeek == 1 || dayOfWeek == 2) {
+    banner.style.display = "block";
+}
+
+document.getElementById("modify").innerHTML = document.lastModified
+document.getElementById("year").innerHTML = new Date().getFullYear()
+
---
+++ js/datetime.js
@@ -0,0 +1,10 @@
+const day = new Date();
+
+var datetime = "Last Sync: " + day.getDate() + "/"
+	+ (day.getMonth()+1)  + "/"
+	+ day.getFullYear() + " || "
+	+ day.getHours() + ":"
+	+ day.getMinutes() + ":"
+	+ day.getSeconds();
+console.log(datetime)
+document.getElementById("date").value = datetime
