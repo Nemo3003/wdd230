88e2a0418e0a98d1b2dc4046b701b872cd7312f8
Ulises Mariano Melgarejo <73831325+Nemo3003@users.noreply.github.com>
2022-03-01 10:42:56 -0300

    number of president added

--- lesson-9/js/prophets.js
+++ lesson-9/js/prophets.js
@@ -13,15 +13,18 @@ fetch(requestURL)
             let h2 = document.createElement('h2');
             let birthdate = document.createElement('p');
             let birthplace = document.createElement('p');
+            let order = document.createElement('p');
             let image = document.createElement('img');
 
             h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
             birthdate.textContent = "Date of Birth: " + prophets[i].birthdate;
             birthplace.textContent = "Place of Birth: " + prophets[i].birthplace;
+            order.textContent = 'President Number: '+ prophets[i].order;
 
             card.appendChild(h2);
             card.appendChild(birthdate);
             card.appendChild(birthplace);
+            card.appendChild(order);
             card.appendChild(image);
 
             image.setAttribute('src', prophets[i].imageurl);
