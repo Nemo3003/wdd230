75d806991d7185c647fe7b242c0fa1f31a17b30b
Ulises Mariano Melgarejo <ulimarian2000@gmail.com>
2022-03-01 19:06:15 -0300

    switch added

--- lesson-9/js/prophets.js
+++ lesson-9/js/prophets.js
@@ -3,7 +3,6 @@ const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-
 fetch(requestURL)
     .then(function (response) {
         return response.json();
-
     })
     .then(function (jsonObject) {
         console.table(jsonObject);
@@ -16,23 +15,25 @@ fetch(requestURL)
             let order = document.createElement('p');
             let image = document.createElement('img');
 
-            h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
-            birthdate.textContent = "Date of Birth: " + prophets[i].birthdate;
-            birthplace.textContent = "Place of Birth: " + prophets[i].birthplace;
-     
-            if(prophets[i].order === 1) {
-                image.setAttribute('alt','Portrait of'+' '+ prophets[i].name+' '+prophets[i].lastname+' - '+ prophets[i].order + "st" + ' ' + "Latter-day president");
+            h2.textContent = `${prophets[i].name} ${prophets[i].lastname}`;
+            birthdate.textContent = `Date of Birth: ${prophets[i].birthdate}`;
+            birthplace.textContent = `Place of Birth: ${prophets[i].birthplace}`;
+
+            switch (prophets[i].order) {
+                case 1:
+                    image.setAttribute('alt',`Portrait of ${prophets[i].name} ${prophets[i].lastname} - ${prophets[i].order}st Latter-day president`);
+                    break;
+                case 2:
+                    image.setAttribute('alt', `Portrait of ${prophets[i].name} ${prophets[i].lastname} - ${prophets[i].order}nd Latter-day president`);
+                    break;
+                case 3:
+                    image.setAttribute('alt', `Portrait of ${prophets[i].name} ${prophets[i].lastname} - ${prophets[i].order}rd Latter-day president`);
+                    break;
+                default:
+                    image.setAttribute('alt', `Portrait of ${prophets[i].name} ${prophets[i].lastname} - ${prophets[i].order}th Latter-day president`);
+                    break;
             }
-            else if(prophets[i].order === 2) {
-                image.setAttribute('alt', 'Portrait of'+' '+ prophets[i].name+' '+prophets[i].lastname+' - '+ prophets[i].order + "nd" + ' ' + "Latter-day president");
-            }
-            else if(prophets[i].order === 3) {
-                image.setAttribute('alt', 'Portrait of'+' '+ prophets[i].name+' '+prophets[i].lastname+' - '+ prophets[i].order + "rd" + ' ' + "Latter-day president");
-            }
-            else{
-                image.setAttribute('alt', 'Portrait of'+' '+ prophets[i].name+' '+prophets[i].lastname+' - '+ prophets[i].order + "th" + ' ' + "Latter-day president");
-            }
-            
+
             card.appendChild(h2);
             card.appendChild(birthdate);
             card.appendChild(birthplace);
@@ -40,7 +41,7 @@ fetch(requestURL)
             card.appendChild(image);
 
             image.setAttribute('src', prophets[i].imageurl);
-        
+
 
             document.querySelector('div.cards').appendChild(card);
         };
