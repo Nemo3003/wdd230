5e043a96c819a81e632b7c0e4b9d8a495f1a4715
Ulises Mariano Melgarejo <ulimarian2000@gmail.com>
2022-03-04 00:11:28 -0300

    something was done

--- chamber/js/dir.js
+++ chamber/js/dir.js
@@ -1,6 +1,6 @@
 const requestURL =" https://nemo3003.github.io/wdd230/chamber/js/data.json";
 //name, address, phone, website, imageurl
-
+function cardView(){
 fetch(requestURL)
     .then(function (response) {
         return response.json();
@@ -36,14 +36,68 @@ fetch(requestURL)
 
 
             document.querySelector('div.cards').appendChild(card);
+
+            
         };
     });
-    window.addEventListener("DOMContentLoaded", () => {
-        document.getElementById("sList").onclick = () => {
-          document.getElementById("cards").classList.remove("grid");
+}
+function listView() {
+    fetch(requestURL)
+    .then(function (response) {
+        return response.json();
+
+    })
+    .then(function (jsonObject) {
+        console.table(jsonObject); // temporary checking for valid response and data parsing
+        const companies = jsonObject['companies'];
+        for (let i = 0; i < companies.length; i++) {
+            let card = document.createElement('section');
+            let title = document.createElement('h2');
+            let address = document.createElement('p');
+            let phone = document.createElement('p');
+            let website = document.createElement('p');
+
+
+            title.textContent = companies[i].name;
+            address.textContent = companies[i].address;
+            phone.textContent = companies[i].phone;
+            website.textContent	= companies[i].website
+
+            card.appendChild(title);
+            card.appendChild(address);
+            card.appendChild(phone);
+            card.appendChild(website);
+
+            title.setAttribute('class', 'title-dir');
+            
+            document.querySelector('div.cards').appendChild(card);
+           
         };
-        document.getElementById("sGrid").onclick = () => {
-          document.getElementById("cards").classList.add("grid");
-        };
-      });
+    });
+}
+
+let buttonList = document.getElementById("sList");
+let buttonGrid = document.getElementById("sGrid");
+
+buttonGrid.addEventListener('click', cardView);
+buttonList.addEventListener('click', listView);
+
+//Jquery code to
+//$(".listView").on('click', function() {
+ //   listView();
+ //   setView('list');
+//  });
+  
+//  $(".gridView").on('click', function() {
+ //    gridView();
+  //   setView('grid');
+ // });
+  
+  //function setView(view) {
+   // localStorage.setItem('view', view);
+  //}
+  
+  //function getView(view) {
+   // return localStorage.getItem('view');
+  //}
     //Nemo's
