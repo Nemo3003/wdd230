4fb4c86eeba9010cb9833567f26eeab75a6b0d49
Ulises Mariano Melgarejo <ulimarian2000@gmail.com>
2022-03-05 20:43:17 -0300

    weather api complete

--- chamber/js/weatherapi.js
+++ chamber/js/weatherapi.js
@@ -5,9 +5,10 @@ fetch(apiURL)
     const iconsrc= `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
     const desc = jsObject.weather[0].description;
     document.getElementById('wind-speed').textContent = `Wind speed: ${jsObject.wind.speed}`;
-    document.getElementById('chill').textContent = `Wind chill${jsObject.wind.deg}`
-    document.getElementById('humidity').textContent = `Humidity:${jsObject.main.humidity}`;
-    document.getElementById('description').textContent = `description${jsObject.weather.description}`;
-    document.getElementById('temp').textContent = `Current temp: ${jsObject.main.temp}`;
-    document.getElementById('icon').textContent = `${jsObject.weather.icon}`;
+    document.getElementById('chill').textContent = `Wind chill: ${jsObject.wind.deg} km/h`
+    document.getElementById('humidity').textContent = `Humidity: ${jsObject.main.humidity}%`;
+    document.getElementById('description').textContent = `${desc}`;
+    document.getElementById('temp').textContent = `Temp ${jsObject.main.temp} C`;
+    document.querySelector('#icon-src').textContent = iconsrc;
+    document.querySelector('#weathericon').setAttribute('src', iconsrc);
   });
