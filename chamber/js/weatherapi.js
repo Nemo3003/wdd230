99ff779cbeaab9c5cfa3f3ff0b640f125b250bf5
Ulises Mariano Melgarejo <ulimarian2000@gmail.com>
2022-03-06 07:54:16 -0300

    purging my css

--- chamber/js/weatherapi.js
+++ chamber/js/weatherapi.js
@@ -6,11 +6,17 @@ fetch(apiURL)
     const desc = jsObject.weather[0].description;
     let temp = jsObject.main.temp;
     let speed = jsObject.wind.speed;
-    document.getElementById('wind-speed').textContent = `Wind speed: ${jsObject.wind.speed}`;
+    const windChillCelsius = (temperature, windSpeed) =>
+ 13.12 +
+  0.6215 * temperature -
+  11.37 * windSpeed ** 0.16 +
+  0.3965 * temperature * windSpeed ** 0.16;
+    let actual_wind_chill = windChillCelsius(temp, speed)
+    document.getElementById('wind-speed').textContent = `Wind speed: ${jsObject.wind.speed} km/h`;
     if (temp > 10 && speed > 177) {
       document.getElementById('chill').textContent = `Wind chill: N/A`
     }else {
-      document.getElementById('chill').textContent = `Wind chill: ${jsObject.wind.deg} km/h`
+      document.getElementById('chill').textContent = `Wind chill: ${Math.round(actual_wind_chill)} C`
     }
     
     document.getElementById('humidity').textContent = `Humidity: ${jsObject.main.humidity}%`;
