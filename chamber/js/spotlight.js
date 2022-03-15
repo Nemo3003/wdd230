990278d18d46339db232eb40d99731d4690bec0c
Nemo3003 <ulimarian2000@gmail.com>
2022-03-14 21:39:57 -0300

    some stuff

--- chamber/js/spotlight.js
+++ chamber/js/spotlight.js
@@ -4,30 +4,24 @@ const requestURL =" https://nemo3003.github.io/wdd230/chamber/data/spotlight.jso
 //name, address, phone, website, imageurl
 
 fetch(requestURL)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (jsonObject) {
-        const spotlights = jsonObject['spotlights']
-        for (let i = 0; i < spotlights.length; i++) {
-            let card = document.createElement('section');
-            let title = document.createElement('h2');
-            let image = document.createElement('img');
-            let description = document.createElement('p');
+    .then((response) => response.json())
+    .then((jsObject) => {
+    const spotlights = jsObject['spotlights'];
+    for (let i = 0; i < spotlights.length; i++) {
+        let title = document.createElement('h2');
+        let image = document.createElement('img');
+        let description = document.createElement('p');
+        
+        title.textContent = spotlights[i].name;
+        description.textContent = spotlights[i].description;
 
-            title.textContent = spotlights[i].title;
-            description.textContent = spotlights[i].description;
+        image.setAttribute('src', spotlights[i].image);
 
-            card.appendChild(title);
-            card.appendChild(image);
-            card.appendChild(description);
-            
-            image.setAttribute('src', spotlights[i].image);
-            title.setAttribute('class', 'title-spot');
-            image.setAttribute('class', 'img-spot');
-            card.setAttribute('id', 'item-spot');
-            image.setAttribute('alt', spotlights[i].title);
+        document.getElementById('spot').appendChild(title);
+        document.getElementById('spot').appendChild(image);
+        document.getElementById('spot').appendChild(description);
 
-            document.getElementById('cards-spot').appendChild(card);
-        };
+    
+
+    }
     });
