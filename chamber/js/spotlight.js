76f7a14bf41160b94ae11bd86c353bebe60321c1
Nemo3003 <ulimarian2000@gmail.com>
2022-03-15 21:21:52 -0300

    json!

--- chamber/js/spotlight.js
+++ chamber/js/spotlight.js
@@ -1,4 +1,12 @@
 "use strict";
+const positions1 = [1,2,3,4,5,6,7,8,9]
+const positions2 = [6,5,4,3,2,1,8,9,7]
+const positions3 = [2,3,1,5,6,4,9,7,8]
+function randomChoice(arr) {
+    return arr[Math.floor(arr.length * Math.random())];
+}
+let choice = randomChoice(positions1);
+console.log(choice)
 
 const requestURL =" https://nemo3003.github.io/wdd230/chamber/data/spotlight.json";
 //name, address, phone, website, imageurl
@@ -7,21 +15,85 @@ fetch(requestURL)
     .then((response) => response.json())
     .then((jsObject) => {
     const spotlights = jsObject['spotlights'];
-    for (let i = 0; i < spotlights.length; i++) {
+    for (let i = 0; i <1; i++) {
+        let choice = randomChoice(positions1);
         let title = document.createElement('h2');
         let image = document.createElement('img');
         let description = document.createElement('p');
         
-        title.textContent = spotlights[i].name;
-        description.textContent = spotlights[i].description;
+        title.textContent = spotlights[choice].name;
+        description.textContent = spotlights[choice].description;
 
-        image.setAttribute('src', spotlights[i].image);
+        image.setAttribute('src', spotlights[choice].image);
 
         document.getElementById('spot').appendChild(title);
         document.getElementById('spot').appendChild(image);
         document.getElementById('spot').appendChild(description);
 
-    
+    }
+    });
+fetch(requestURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
+    const spotlights = jsObject['spotlights'];
+    let choice = randomChoice(positions2);
+    for (let i = 0; i <1; i++) {
+        let title = document.createElement('h2');
+        let image = document.createElement('img');
+        let description = document.createElement('p');
+        
+        title.textContent = spotlights[choice].name;
+        description.textContent = spotlights[choice].description;
+
+        image.setAttribute('src', spotlights[choice].image);
+
+        document.getElementById('spot1').appendChild(title);
+        document.getElementById('spot1').appendChild(image);
+        document.getElementById('spot1').appendChild(description);
 
     }
     });
+    fetch(requestURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
+    const spotlights = jsObject['spotlights'];
+    let choice = randomChoice(positions3);
+    for (let i = 0; i <1; i++) {
+        let title = document.createElement('h2');
+        let image = document.createElement('img');
+        let description = document.createElement('p');
+        
+        title.textContent = spotlights[choice].name;
+        description.textContent = spotlights[choice].description;
+
+        image.setAttribute('src', spotlights[choice].image);
+
+        document.getElementById('spot1').appendChild(title);
+        document.getElementById('spot1').appendChild(image);
+        document.getElementById('spot1').appendChild(description);
+
+    }
+    });
+
+fetch(requestURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
+    const spotlights = jsObject['spotlights'];
+    for (let i = 0; i <1; i++) {
+        let title = document.createElement('h2');
+        let image = document.createElement('img');
+        let description = document.createElement('p');
+        
+        title.textContent = spotlights[choice].name;
+        description.textContent = spotlights[choice].description;
+
+        image.setAttribute('src', spotlights[choice].image);
+
+        document.getElementById('spot2').appendChild(title);
+        document.getElementById('spot2').appendChild(image);
+        document.getElementById('spot2').appendChild(description);
+
+    }
+    });
+
+
