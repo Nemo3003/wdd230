3b36a1504f6cb03d1c569e6b0225a0836fec465f
“Nemo3003” <ulimarian2000@gmail.com>
2022-03-24 11:46:44 -0300

    yeah

--- temple/js/temples.js
+++ temple/js/temples.js
@@ -1,6 +1,15 @@
 "use strict";
 
 const url = 'https://nemo3003.github.io/wdd230/temple/data/temples.json';
+
+let positions1 = [0,1,2]
+let positions2 = [3,4,5]
+let positions3 = [6,7,8]
+function randomChoice(arr) {
+    return arr[Math.floor(arr.length * Math.random())];
+}
+
+
 fetch(url)
 //dedication, imageUrl, location, templeName
 .then(response => response.json())
@@ -8,27 +17,69 @@ fetch(url)
 .then((jsObject) => {
     const temples = jsObject['temples'];
     for (let i = 0; i < 1; i++) {
+        let choice = randomChoice(positions1);
         let card = document.createElement('div');
         let name = document.createElement('h2');
         let image = document.createElement('img');
         let dedication = document.createElement('p');
         let location = document.createElement('p');
 
-        name.textContent = temples[i].templeName;
-        image.setAttribute('src', temples[i].imageUrl);
-        image.setAttribute('alt', temples[i].templeName);
-        dedication.textContent = temples[i].dedication;
-        location.textContent = temples[i].location;
+        name.textContent = temples[choice].templeName;
+        image.setAttribute('src', temples[choice].imageUrl);
+        image.setAttribute('alt', temples[choice].templeName);
+        dedication.textContent = temples[choice].dedication;
+        location.textContent = temples[choice].location;
 
         card.appendChild(name);
         card.appendChild(image);
         card.appendChild(dedication);
         card.appendChild(location);
 
-        document.getElementById('temple').appendChild(card);
+        document.getElementById('temple1').appendChild(card);
+      
 
 
     }
+    for (let i = 0; i < 1; i++) {
+        let choice = randomChoice(positions2);
+        let card = document.createElement('div');
+        let name = document.createElement('h2');
+        let image = document.createElement('img');
+        let dedication = document.createElement('p');
+        let location = document.createElement('p');
+
+        name.textContent = temples[choice].templeName;
+        image.setAttribute('src', temples[choice].imageUrl);
+        image.setAttribute('alt', temples[choice].templeName);
+        dedication.textContent = temples[choice].dedication;
+        location.textContent = temples[choice].location;
+
+        card.appendChild(name);
+        card.appendChild(image);
+        card.appendChild(dedication);
+        card.appendChild(location);
+
+        document.getElementById('temple2').appendChild(card);}
+        for (let i = 0; i < 1; i++) {
+            let choice = randomChoice(positions3);
+            let card = document.createElement('div');
+            let name = document.createElement('h2');
+            let image = document.createElement('img');
+            let dedication = document.createElement('p');
+            let location = document.createElement('p');
+    
+            name.textContent = temples[choice].templeName;
+            image.setAttribute('src', temples[choice].imageUrl);
+            image.setAttribute('alt', temples[choice].templeName);
+            dedication.textContent = temples[choice].dedication;
+            location.textContent = temples[choice].location;
+    
+            card.appendChild(name);
+            card.appendChild(image);
+            card.appendChild(dedication);
+            card.appendChild(location);
+    
+            document.getElementById('temple3').appendChild(card);}
 
 
 });
--- temple/js/weather.js
+++ temple/js/weather.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=3429721&appid=7d25f81a71306eb3ef3609d311f3b054&units=imperial";
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=3429721&appid=7d25f81a71306eb3ef3609d311f3b054&units=imperial";  
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
@@ -14,16 +14,16 @@ fetch(apiURL)
   11.37 * windSpeed ** 0.16 +
   0.3965 * temperature * windSpeed ** 0.16;
     let actual_wind_chill = windChillCelsius(temp, speed)
-    document.getElementById('wind-speed').textContent = `Wind speed: ${jsObject.wind.speed} km/h`;
+    document.getElementById('wind-speed').textContent = `Wind speed: ${jsObject.wind.speed} mp/h`;
     if (temp > 10 || speed > 177) {
       document.getElementById('chill').textContent = `Wind chill: N/A`
     }else {
-      document.getElementById('chill').textContent = `Wind chill: ${Math.round(actual_wind_chill)} C`
+      document.getElementById('chill').textContent = `Wind chill: ${Math.round(actual_wind_chill)} °F`
     }
     
     document.getElementById('humidity').textContent = `Humidity: ${jsObject.main.humidity}%`;
     document.getElementById('description').textContent = `Conditions: ${desc}`;
-    document.getElementById('temp').textContent = `Temp ${jsObject.main.temp} C`;
+    document.getElementById('temp').textContent = `Temp ${jsObject.main.temp} °F`;
     document.querySelector('#icon-src').textContent = jsObject.weather.icon;
     document.querySelector('#weathericon').setAttribute('src', iconsrc);
   });
