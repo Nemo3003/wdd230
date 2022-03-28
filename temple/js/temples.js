0da72432f9a3351b8415728602f710fcd76c1d58
Ulises Mariano Melgarejo <73831325+Nemo3003@users.noreply.github.com>
2022-03-28 16:03:47 -0300

    Update temples.js

--- temple/js/temples.js
+++ temple/js/temples.js
@@ -5,16 +5,33 @@ const cput = (array)=>{
         const h4 = document.createElement('h4');
         const h4_2 = document.createElement('h4');
         const img = document.createElement('img');
+        const voting = document.createElement('div');
+        const like = document.createElement('button');
+        const dislike = document.createElement('button')
         h3.innerHTML = temple.templeName;
         h4.innerHTML = temple.location;
         h4_2.innerHTML = temple.dedicated;
+
+        like.setAttribute('type', 'like');
+        dislike.setAttribute('type', 'dislike');
+        like.setAttribute('onclick', 'Like()');
+        dislike.setAttribute('onclick', 'Dislike()');
+        like.textContent = 'Like';
+        dislike.textContent = 'Dislike';
+
         img.src = temple.imageUrl;
         img.alt = temple.templeName;
         img.setAttribute('class', 'temple-image');
+
         article.appendChild(h3);
         article.appendChild(h4);
         article.appendChild(h4_2);
         article.appendChild(img);
+        
+        voting.appendChild(like);
+        voting.appendChild(dislike);
+        article.appendChild(voting);
+
         document.querySelector('#temple').appendChild(article);
     }, this);
  }
@@ -28,3 +45,9 @@ fetch(url)
     temples = temples_list;
     cput(temples);
 })
+const Like = () =>{
+    console.log('I like it!')
+}
+const Dislike = () => {
+    console.log('I do not like this!!!')
+}
