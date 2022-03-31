366f96bde65539530a78815b4588bd4946d223b5
“Nemo3003” <ulimarian2000@gmail.com>
2022-03-31 20:22:29 -0300

    ups

--- temple/js/weather.js
+++ temple/js/weather.js
@@ -6,18 +6,18 @@
     .then((response) => response.json())
     .then((jsObject) => {
         const time = jsObject.list.filter(x => x.dt_txt.includes('18:00:00'));
-        console.log(time);
         const weekdays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
         let day = 0;
         time.forEach(forecast => {
             if (day > 2)
-            return;
+            return
             let thedate = new Date(forecast.dt_txt);
-            const imagesrc = 'https://openweathermap.org/img/w/' + forecast.weather[0].icon + '.png';
+            const imagesrc = `https://openweathermap.org/img/w/${forecast.weather[0].icon}.png`;
             const desc = forecast.weather[0].description;
             document.querySelector(`#weekday${day+1}`).textContent = weekdays[thedate.getDay()];
             document.querySelector(`#ftemp${day+1}`).textContent = forecast.main.temp.toFixed(0) + "°F";
+
             document.querySelector(`#icon${day+1}`).setAttribute('src', imagesrc);
             document.querySelector(`#icon${day+1}`).setAttribute('alt', desc);
             day++;
