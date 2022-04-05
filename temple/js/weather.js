7a8f1d075227dd07b3d1f60f236693bc748ce492
“Nemo3003” <ulimarian2000@gmail.com>
2022-04-04 23:20:44 -0300

    almost there

--- temple/js/weather.js
+++ temple/js/weather.js
@@ -1,7 +1,7 @@
 "use strict";
 
 
-    const fapiURL = "https://api.openweathermap.org/data/2.5/forecast?id=4348599&appid=7d25f81a71306eb3ef3609d311f3b054&units=imperial";
+    const fapiURL = "https://api.openweathermap.org/data/2.5/forecast?id=5014130&appid=7d25f81a71306eb3ef3609d311f3b054&units=imperial";
     fetch(fapiURL)
     .then((response) => response.json())
     .then((jsObject) => {
@@ -23,6 +23,3 @@
             day++;
         });
     });
-  // не кради! это моя собственная работа!
-  //不要盗窃！这是我自己的工作！
-  //ne pas voler! c'est mon propre travail!
